a0cf2cd4327f820ac47c49dcb11e817aa70752f0
Dgibbins <nayr_ryan@hotmail.com>
2017-01-18 13:12:09 -0500

    Attributes :name :food :meal_time added

--- ex2.rb
+++ ex2.rb
@@ -1,5 +1,5 @@
 class Cat
-  attr_reader :name, :food, :meal_time
+  attr_reader :name, :preffered_food, :meal_time
   def initialize(name, preffered_food, meal_time)
     @name = name
     @preffered_food = preffered_food
